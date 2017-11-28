3691f2ebfcc42fe5ddfef2d126c9f44a7c60f401
AndrewBills <31480930+AndrewBills@users.noreply.github.com>
2017-11-28 16:30:31 -0700

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -1,59 +1,55 @@
 //
-// Created by Hugo Valle on 11/12/17.
+// Created by Andrew Bills on 11/12/17.
 //
 #include <memory>
 #include "MyString.h"
+#include <algorithm>
 
 using namespace std;
 
 //  assigning a value to a unique pointer   http://www.cplusplus.com/reference/memory/unique_ptr/release/
 MyString::MyString ()
 {
-    internalCString* = "/0";
+    nlength=1;
+    internalCString = make_unique<char[]>(nlength);
+    internalCString[0] = '\0';
 }
 
 //  using the equals sign with a unique pointer   http://www.cplusplus.com/reference/memory/unique_ptr/operator=/
 MyString::MyString(const char *cString)
 {
-    //internalCString.reset (new char[]);
-    cString = move(cString);
-    //internalCString* = std::unique_ptr<char[]>( );
-
+    nlength = 1;
+    while(*(cString+nlength))
+        nlength++;
+    internalCString = make_unique<char[]>(nlength+1);
+    for(int len = 0; len < (nlength); len ++)
+        internalCString[len] = cString[len];
+    internalCString[(nlength+1)] = '\0';
 }
 //   getting a unique pointer   http://www.cplusplus.com/reference/memory/unique_ptr/get/
-char MyString::*getInternalCString () const
+char *MyString::getInternalCString () const
 {
-    return *internalCString.get();
-    
-     // I belive that i need to find out to add a null character to get this to work
+    return internalCString.get();
 }
-
 // explains several ways to acomplish the task in this fourm https://stackoverflow.com/questions/4180818/finding-the-length-of-a-character-array-in-c
 //returns the size of a character array using a pointer to the first element of the character array
-int getNlength () const
+int MyString::getNlength () const
 {
-  unsigned int len=0;
-  while(*(internalCString+len) ) len++;
-  return len;
+    return nlength;
 }
 // at this site are several ways to revese char array http://www.cplusplus.com/forum/general/14951/
-void reverseit ()
+void MyString::reverseit ()
 {
-    unsigned int len=0;
-    while(*(internalCString+len) ) len++;
-    
-    char* newStr = (char*)malloc(len);
-    
-    for (int i=0;i<len/len;i++)
-       newStr [i] = str [len/len-1-i];
-
+    for (int i=0, n = getNlength(); i<n/2; i++)
+       swap(internalCString[i], internalCString[n-i-1]);
 }
 
 int compareStr (const MyString &lhs, const MyString &rhs)
 {
-    
+
 }
-friend ostream & operator<< (ostream &os, const MyString &myString)
+ostream & operator<< (ostream &os, const MyString &myString)
 {
-    
+    os << myString.getInternalCString();
+    return os;
 }
